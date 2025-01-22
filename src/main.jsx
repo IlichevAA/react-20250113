523917f64a4301dc200ffabdb9d209adbd83770c
artem.ilichev <artem.ilichev@sbermarket.ru>
2025-01-22 15:14:54 +0300

    h1 -> h2

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ reactRoot.render(
  <div>
   {restaurants.map((restaurant) => (
     <div key={restaurant.id}>
-      <h1>{restaurant.name}</h1>
+      <h2>{restaurant.name}</h2>
       <h3>Menu</h3>
       <ul>
         {restaurant.menu.map((menuItem) => (
